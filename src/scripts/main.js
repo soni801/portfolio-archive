6361ef92da29e78c3632cc270f1078e5e4a15e4f
soni801 <32430506+soni801@users.noreply.github.com>
2021-08-22 11:33:36 +0200

    Add header highlighting

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
-$("#header").load("/sections/header.html");
+$("#header").load("/sections/header.html", function ()
+{
+    document.querySelector("#header a").classList.add("highlighted");
+    document.querySelectorAll("#header a").forEach(function (e)
+    {
+        e.addEventListener("click", function ()
+        {
+            document.querySelectorAll("#header a").forEach(function (e)
+            {
+                e.classList.remove("highlighted");
+            });
+            e.classList.add("highlighted");
+        });
+    });
+});
