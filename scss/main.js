30ce54bc010e49377b489e6b352f969434010456
soni801 <samuel.aasen@hotmail.com>
2021-09-13 16:57:13 +0000

    Update gh-pages to output generated at 1562fc8

--- scss/main.js
+++ scss/main.js
@@ -117,140 +117,79 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"assets/projects.js":[function(require,module,exports) {
-"use strict";
+})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.projects = void 0;
-var projects = {
-  "school": [{
-    "image": "/assets/images/projects/school/weather-station.jpg",
-    "subject": "Konseptutvikling og programmering + Teknologiforståelse",
-    "title": "Værstasjon",
-    "content": "Her skulle vi måle forskjellige værdata som temperatur, lufttrykk og luftfuktighet, og laste det opp til en database.",
-    "date": "26. apr. 2021"
-  }, {
-    "image": "/assets/images/projects/school/app.jpg",
-    "subject": "Konseptutvikling og programmering",
-    "title": "App",
-    "content": "Vi skulle utvikle en app for en valgfri kunde. Vi valgte å lage en app for Kaffe Diem, en kaffebar på skolen.",
-    "date": "26. mar. 2021"
-  }, {
-    "image": "/assets/images/projects/school/raspberry-pi.jpg",
-    "subject": "Teknologiforståelse",
-    "title": "Raspberry Pi",
-    "content": "Her skulle vi sette opp og teste forskjellige ting med en Raspberry Pi minidatamaskin. Vi koblet for eksempel opp lys og programmerte de til å lyse forskjellig over tid eller hvis man trykket på knapper.",
-    "date": "26. feb 2021"
-  }, {
-    "image": "/assets/images/projects/school/database.jpg",
-    "subject": "Konseptutvikling og programmering",
-    "title": "Database",
-    "content": "Dette var en oppgave om å lage en database og kjøre den lokalt, og å kunne legge inn og hente ut informasjon fra den.",
-    "date": "22. feb. 2021"
-  }, {
-    "image": "/assets/images/projects/school/covid-diary.jpg",
-    "subject": "Produksjon og historiefortelling",
-    "title": "Koronadagbok",
-    "content": "Vi skulle lage en film der vi dokumenterte livet vårt gjennom en uke under koronasituasjonen. Den inkluderer skolehverdagen, praksis i bedrift, og hva jeg gjør på fritiden.",
-    "date": "8. jan. 2021"
-  }, {
-    "image": "/assets/images/projects/school/mindstorms.jpg",
-    "subject": "Konseptutvikling og programmering",
-    "title": "LEGO Mindstorms",
-    "content": "Dette var et prosjekt der vi ble delt inn i grupper og fikk i oppgave å programmere en LEGO-robot til å gjøre forskjellige oppgaver. Dette var ting som å bevege seg på en spesiell måte eller spille av en spesifik lyd.",
-    "date": "10. des. 2020"
-  }, {
-    "image": "/assets/images/projects/school/stream.jpg",
-    "subject": "Konseptutvikling og programmering",
-    "title": "Veldedighetsstream",
-    "content": "Her skulle vi planlegge og gjennomføre en veldedighetsstream for å samle inn penger til Bring Children From Streets. Her gjorde vi mye forskjellig. Hovedsakelig gaming, men også kommentering, giveaways og spesielle ting når vi nådde et donasjonsmål.",
-    "date": "9. des. 2020"
-  }, {
-    "image": "/assets/images/projects/school/advertisement.jpg",
-    "subject": "Konseptutvikling og programmering + Produksjon og historiefortelling",
-    "title": "Reklamefilm",
-    "content": "Vi skulle lage en reklamefilm for å reklamere for linjen vi går på. Her viste vi fram litt fra hvert fag, litt fra skolefrokosten og litt fra Kaffe Diem. Vi gjorde også et par intervuer med elever i klassen.",
-    "date": "17. nov. 2020"
-  }, {
-    "image": "/assets/images/projects/school/superfood.jpg",
-    "subject": "Naturfag",
-    "title": "Supermat",
-    "content": "Her skulle vi lage en fiksjonell \"supermat\" som inneholdt akkurat riktig mengde næringsstoffer og mineraler, og som var bærekraftig. Jeg lagde min 'råbar'.",
-    "date": "30. okt. 2020"
-  }, {
-    "image": "/assets/images/projects/school/photo-series.jpg",
-    "subject": "Teknologiforståelse + Produksjon og historiefortelling",
-    "title": "Bildeserie",
-    "content": "Denne oppgaven gikk ut på å fortelle en historie ved hjelp av bilder. Vi skulle både ta og redigere 4 bilder for å kunne fortelle en meningsfull historie ut i fra dem. Jeg ville prøve å fortelle en historie om forsøpling, men det er selvfølgelig opp til enkeltpersonen å tolke historien.",
-    "date": "18. sep. 2020"
-  }, {
-    "image": "/assets/images/projects/school/website.jpg",
-    "subject": "Konseptutvikling og programmering",
-    "title": "Nettside",
-    "content": "Vi skulle lage en nettside for å vise hva vi gjør på linjen, som en litt annerledes skrivebok. Det er den nettsiden du er på nå. Denne nettsiden har gått gjennom mange iterasjoner og har til slutt landet på det den er i dag. Dette er høyst sannsynlig det skolerelaterte prosjektet jeg har jobbet mest på.",
-    "date": "20. aug. 2020"
-  }],
-  "personal": [{
-    "image": "/assets/images/projects/personal/graphide.jpg",
-    "title": "graphide studios",
-    "content": "graphide studios er et lite programvareutviklingsstudio som jeg og noen venner laget."
-  }, {
-    "image": "/assets/images/projects/personal/redsea.jpg",
-    "title": "Redsea",
-    "content": "Redsea Productions er et lite programvareutviklingsstudio som jeg og noen venner laget."
-  }, {
-    "image": "/assets/images/projects/personal/linerider.jpg",
-    "title": "Line Rider",
-    "content": "Line Rider er et spill der man produserer videoer av en person som kjører på kjelke på linjer man selv har tegnet. Du kan finne mine Line Rider videoer på min YouTube-kanal."
-  }, {
-    "image": "/assets/images/projects/personal/cubing.jpg",
-    "title": "Kubing",
-    "content": "Kubing går ut på å løse en Rubik's kube så fort som mulig. Min rekord er 12.89 sekunder."
-  }]
-};
-exports.projects = projects;
-},{}],"scripts/index.js":[function(require,module,exports) {
-"use strict";
-
-var _projects = require("/assets/projects.js");
-
-function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-// Load recent projects
-var _iterator = _createForOfIteratorHelper(_projects.projects.school),
-    _step;
-
-try {
-  for (_iterator.s(); !(_step = _iterator.n()).done;) {
-    var project = _step.value;
-    document.querySelector("#school-projects").innerHTML += "\n        <div>\n            <img src=\"".concat(project.image, "\" alt=\"Image of ").concat(project.title, "\">\n            <h2>").concat(project.title, "</h2>\n            <p>").concat(project.subject, "</p>\n            <p>").concat(project.content, "</p>\n            <p>").concat(project.date, "</p>\n        </div>\n    ");
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
   }
-} catch (err) {
-  _iterator.e(err);
-} finally {
-  _iterator.f();
+
+  return bundleURL;
 }
 
-var _iterator2 = _createForOfIteratorHelper(_projects.projects.personal),
-    _step2;
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
 
-try {
-  for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
-    var _project = _step2.value;
-    document.querySelector("#personal-projects").innerHTML += "\n        <div>\n            <img src=\"".concat(_project.image, "\" alt=\"Image of ").concat(_project.title, "\">\n            <h2>").concat(_project.title, "</h2>\n            <p>").concat(_project.subject, "</p>\n            <p>").concat(_project.content, "</p>\n            <p>").concat(_project.date, "</p>\n        </div>\n    ");
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
   }
-} catch (err) {
-  _iterator2.e(err);
-} finally {
-  _iterator2.f();
+
+  return '/';
 }
-},{"/assets/projects.js":"assets/projects.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"scss/main.scss":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -454,5 +393,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","scripts/index.js"], null)
-//# sourceMappingURL=/scripts/index.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/scss/main.js.map
