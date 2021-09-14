63cb6c159ae1f3fc3d4aab50133befad0bf42536
soni801 <32430506+soni801@users.noreply.github.com>
2021-09-14 18:33:46 +0200

    Update school project images

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -132,13 +132,13 @@ var projects = {
     "content": "Her skulle vi måle forskjellige værdata som temperatur, lufttrykk og luftfuktighet, og laste det opp til en database.",
     "date": "26. apr. 2021"
   }, {
-    "image": "/assets/images/projects/school/app.jpg",
+    "image": "/assets/images/projects/school/app.jpg?v=2",
     "subject": "Konseptutvikling og programmering",
     "title": "App",
     "content": "Vi skulle utvikle en app for en valgfri kunde. Vi valgte å lage en app for Kaffe Diem, en kaffebar på skolen.",
     "date": "26. mar. 2021"
   }, {
-    "image": "/assets/images/projects/school/raspberry-pi.jpg",
+    "image": "/assets/images/projects/school/raspberry-pi.jpg?v=2",
     "subject": "Teknologiforståelse",
     "title": "Raspberry Pi",
     "content": "Her skulle vi sette opp og teste forskjellige ting med en Raspberry Pi minidatamaskin. Vi koblet for eksempel opp lys og programmerte de til å lyse forskjellig over tid eller hvis man trykket på knapper.",
@@ -150,13 +150,13 @@ var projects = {
     "content": "Dette var en oppgave om å lage en database og kjøre den lokalt, og å kunne legge inn og hente ut informasjon fra den.",
     "date": "22. feb. 2021"
   }, {
-    "image": "/assets/images/projects/school/covid-diary.jpg",
+    "image": "/assets/images/projects/school/covid-diary.jpg?v=2",
     "subject": "Produksjon og historiefortelling",
     "title": "Koronadagbok",
     "content": "Vi skulle lage en film der vi dokumenterte livet vårt gjennom en uke under koronasituasjonen. Den inkluderer skolehverdagen, praksis i bedrift, og hva jeg gjør på fritiden.",
     "date": "8. jan. 2021"
   }, {
-    "image": "/assets/images/projects/school/mindstorms.jpg",
+    "image": "/assets/images/projects/school/mindstorms.jpg?v=2",
     "subject": "Konseptutvikling og programmering",
     "title": "LEGO Mindstorms",
     "content": "Dette var et prosjekt der vi ble delt inn i grupper og fikk i oppgave å programmere en LEGO-robot til å gjøre forskjellige oppgaver. Dette var ting som å bevege seg på en spesiell måte eller spille av en spesifik lyd.",
@@ -168,7 +168,7 @@ var projects = {
     "content": "Her skulle vi planlegge og gjennomføre en veldedighetsstream for å samle inn penger til Bring Children From Streets. Her gjorde vi mye forskjellig. Hovedsakelig gaming, men også kommentering, giveaways og spesielle ting når vi nådde et donasjonsmål.",
     "date": "9. des. 2020"
   }, {
-    "image": "/assets/images/projects/school/advertisement.jpg",
+    "image": "/assets/images/projects/school/advertisement.jpg?v=2",
     "subject": "Konseptutvikling og programmering + Produksjon og historiefortelling",
     "title": "Reklamefilm",
     "content": "Vi skulle lage en reklamefilm for å reklamere for linjen vi går på. Her viste vi fram litt fra hvert fag, litt fra skolefrokosten og litt fra Kaffe Diem. Vi gjorde også et par intervuer med elever i klassen.",
@@ -186,7 +186,7 @@ var projects = {
     "content": "Denne oppgaven gikk ut på å fortelle en historie ved hjelp av bilder. Vi skulle både ta og redigere 4 bilder for å kunne fortelle en meningsfull historie ut i fra dem. Jeg ville prøve å fortelle en historie om forsøpling, men det er selvfølgelig opp til enkeltpersonen å tolke historien.",
     "date": "18. sep. 2020"
   }, {
-    "image": "/assets/images/projects/school/website.jpg",
+    "image": "/assets/images/projects/school/website.jpg?v=2",
     "subject": "Konseptutvikling og programmering",
     "title": "Nettside",
     "content": "Vi skulle lage en nettside for å vise hva vi gjør på linjen, som en litt annerledes skrivebok. Det er den nettsiden du er på nå. Denne nettsiden har gått gjennom mange iterasjoner og har til slutt landet på det den er i dag. Dette er høyst sannsynlig det skolerelaterte prosjektet jeg har jobbet mest på.",
@@ -307,7 +307,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55687" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60445" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
