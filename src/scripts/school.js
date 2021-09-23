002b14198cfdf0e4790e32d8ed3ffb7584694ce1
Soni <soni801_@outlook.com>
2021-09-23 17:39:13 +0200

    Add project content

--- src/scripts/school.js
+++ src/scripts/school.js
@@ -4,6 +4,17 @@ import { projects } from "../assets/projects.js";
 projects.school.forEach(function (e)
 {
     document.querySelector("#projects-list").innerHTML += `
-        <h1>${e.title}</h1>
+        <div>
+            <h1>${e.title}</h1>
+            <p>${e.content}</p>
+        </div>
     `;
+});
+
+document.querySelectorAll("#projects-list h1").forEach(function (e)
+{
+    e.addEventListener("click", function ()
+    {
+        e.nextElementSibling.style.display = e.nextElementSibling.style.display === "initial" ? "none" : "initial";
+    });
 });
