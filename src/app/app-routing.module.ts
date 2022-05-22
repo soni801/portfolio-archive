e1dda29359f212e5849ab0e2b5106d3114bc8012
Soni <soni801_@outlook.com>
2022-05-22 20:02:33 +0200

    chore: add base

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+import {SchoolComponent} from "./school/school.component";
+import {PersonalComponent} from "./personal/personal.component";
+
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'school', component: SchoolComponent },
+    { path: 'personal', component: PersonalComponent },
+    { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SchoolComponent } from './school/school.component';
+import { PersonalComponent } from './personal/personal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NotFoundComponent,
+    SchoolComponent,
+    PersonalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
